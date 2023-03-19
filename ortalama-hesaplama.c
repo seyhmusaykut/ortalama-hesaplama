4eb891a6375d0215dd6271e71f40c439e50ef6fe
Şeyhmus Aykut <89939854+seyhmusaykut23@users.noreply.github.com>
2023-03-19 20:19:11 +0300

    Add files via upload

---
+++ ortalama-hesaplama.c
@@ -0,0 +1,88 @@
+/*
+Öðrencinin derste aldýðý iki ara sýnav, bir ödev ve bir final sýnavý notlarýný okuyan ve girilen notlara göre harf notu hesaplayan program.
+
+*Notlar 100 üzerinden verilmiþtir.++
+*Final sýnavýndan 50'nin altýnda not alan öðrenci direkt F notu almaktadýr.
+*Programa 0-100 aralýðý dýþýnda not girildiðinde uyarý verecektir. +
+
+Sýnavlarýn baþarý notuna katkýsý þöyledir:
+
+1. Ara Sýnav %15++
+2. Ara Sýnav %20++
+Ödev %20++
+Final %45++
+
+Baþarý notuna karþýlýk gelen harf notlarý:
+
+85-100 A++
+75-84 B++
+60-74 C++
+50-59 D++
+49 ve altý F++
+
+*/
+#include <stdio.h>
+main()
+{
+	int a1,f,a2,o1;
+	printf("1. ara sinav notunuz:");
+	scanf("%d",&a1);
+	if(a1>100){
+		printf("Not 100'den buyuk olamaz");
+	}
+	else if(a1<0){
+		printf("Not 0'dan kucuk olamaz");
+	}
+	printf("2. ara sinav notunuz:");
+	scanf("%d",&a2);
+	if(a2>100){
+		printf("Not 100'den buyuk olamaz");
+	}
+	else if(a2<0){
+		printf("Not 0'dan kucuk olamaz");
+	}
+	printf("odev notunuz:");
+	scanf("%d",&o1);
+	if(o1>100){
+		printf("Not 100'den buyuk olamaz");
+	}
+	else if(o1<0){
+		printf("Not 0'dan kucuk olamaz");
+	}
+	printf("final notunuz:");
+	scanf("%d",&f);
+	if(f>100){
+		printf("Not 100'den buyuk olamaz");
+	}
+	else if(f<0){
+		printf("Not 0'dan kucuk olamaz");
+	}
+	if(f<50) 
+	{
+		goto islem;
+	}
+	double ort = (a1 * 0.15) + (a2 * 0.2) + (o1 * 0.2) + (f * 0.45);
+	printf("Ortalamaniz = %.0lf",ort);
+	if(ort<=100 && ort>=85)
+	{
+		printf("Harf Notu: A");
+	}
+	else if(ort<=84 && ort>=75)
+	{
+		printf("Harf Notu: B");
+	}
+	else if(ort<=74 && ort>=60)
+	{
+		printf("Harf Notu: C");
+	}
+	else if(ort<=59 && ort>=50)
+	{
+		printf("Harf Notu: D");
+	}
+	else if(ort<=49)
+	{
+		islem:
+		printf("Harf Notu: F");
+	}
+	getch();
+}
